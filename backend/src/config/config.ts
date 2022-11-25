ab08284d2423ac1acc2445150f6445d93d23a372
hoTiwe <zerftiv125@gmail.com>
2022-11-26 00:50:05 +0300

    added registration v2

---
+++ backend/src/config/config.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const config = {
+    PORT: Number(process.env.PORT),
+    DB_NAME: String(process.env.DB_NAME),
+    DB_USER: String(process.env.DB_USER),
+    DB_PASS: String(process.env.DB_PASS),
+    DB_HOST: String(process.env.DB_HOST),
+    DB_PORT: Number(process.env.DB_PORT)
+}
+
+export default config
