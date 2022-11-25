1c110c0a3fd2c5a73af448b24f1d43362896ca57
hoTiwe <zerftiv125@gmail.com>
2022-11-26 02:30:13 +0300

    added list 3

---
+++ frontend/app/src/main/java/com/example/oggetto/EnterPersonInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.oggetto
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EnterPersonInfoActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_person_info_2)
+    }
+}
