5403bb10309e14906797b8c6c0b2eecac1e0b4e9
Electr1k <110545080+Electr1k@users.noreply.github.com>
2022-11-26 02:28:30 +0300

    added registration activity 3 and user class

---
+++ frontend/app/src/main/java/com/example/oggetto/Model/User.kt
@@ -0,0 +1,16 @@
+package com.example.oggetto.Model
+
+import java.io.File
+
+data class User (
+    var email: String? = null,
+    var password: String? = null,
+    var name: String? = null,
+    var profession: Int? = null,
+    var wordAboutSelf: String? = null,
+    var image: File? = null,
+    var hobbies: Set<Int>  = mutableSetOf<Int>(),
+    var link_inst: String? = null,
+    var link_tg: String? = null,
+    var link_vk: String? = null
+)
